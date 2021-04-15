1b5849b6b96c610774a00236e9574dd10e6e16cb
VInicius <duartevinicius91@gmail.com>
2021-04-15 15:32:34 -0300

    Create project

---
+++ src/main/java/br/com/datapoa/DatapoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.datapoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapoaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatapoaApplication.class, args);
+  }
+
+}
